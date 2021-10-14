75e92d72b9e515c0d5ee09288d212432f524c01a
Sundaralingam <sundaralingam.murugathithan@oracle.com>
2021-10-14 14:55:59 +0530

    modified to run test T001

--- src/test/java/com/oracle/ICICI/common/runners/DevelopmentRunner.java
+++ src/test/java/com/oracle/ICICI/common/runners/DevelopmentRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        features = { "src/test/java/com/oracle/ICICI/HCM/features" }, 
+        tags = " @T001", features = { "src/test/java/com/oracle/ICICI/HCM/features" },
         		plugin = { "pretty", "json:target/json/report.json",
         				"html:target/html/ofs"}, glue = {  "com.oracle.ICICI.common.steps",
                         "com.oracle.ICICI.common.runners", "com.oracle.ICICI.HCM.steps" }, dryRun = false)
